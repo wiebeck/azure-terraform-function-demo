292f247a8898001640ceac557f4aa130e127e1ba
Oliver Wiebeck <wiebeck@riege.com>
2021-01-12 15:45:13 +0100

    Remove comments

--- infrastructure/function-app.tf
+++ infrastructure/function-app.tf
@@ -22,8 +22,6 @@ resource "azurerm_function_app" "func" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
-  // This is deprecated but using only storage_account_name and storage_account_access_key
-  //storage_connection_string = azurerm_storage_account.storage.primary_connection_string
   storage_account_name       = azurerm_storage_account.storage.name
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
   version                    = "~3"
