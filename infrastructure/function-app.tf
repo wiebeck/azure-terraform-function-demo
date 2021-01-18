3b28379c1834e4aaff39073d2fabd014ed3f7d86
Oliver Wiebeck <wiebeck@riege.com>
2021-01-18 13:54:17 +0100

    Switch to Linux Consumption Plan

--- infrastructure/function-app.tf
+++ infrastructure/function-app.tf
@@ -2,8 +2,8 @@ resource "azurerm_app_service_plan" "asp" {
   name                = "${var.prefix}-plan"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
-  // default is "Windows" operating system
-  kind = "FunctionApp"
+  kind                = "FunctionApp"
+  reserved            = true
   sku {
     tier = "Dynamic"
     size = "Y1"
@@ -18,18 +18,18 @@ resource "azurerm_application_insights" "appinsights" {
 }
 
 resource "azurerm_function_app" "func" {
-  name                = "${var.prefix}-func"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  name                       = "${var.prefix}-func"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.asp.id
   storage_account_name       = azurerm_storage_account.storage.name
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  os_type                    = "linux"
   version                    = "~3"
-  // TODO: How to set the Java runtime version?
-  // the following was recommended on https://stackoverflow.com/a/64533932 but doesn't work: "The parameter LinuxFxVersion has an invalid value."
-  // site_config {
-  //   linux_fx_version = "11"
-  // }
+  enable_builtin_logging     = false
+  site_config {
+    linux_fx_version = "JAVA|11"
+  }
   app_settings = {
     FUNCTIONS_WORKER_RUNTIME       = "java"
     APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.appinsights.instrumentation_key
